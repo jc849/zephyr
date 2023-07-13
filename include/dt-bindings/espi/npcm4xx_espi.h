6221b294a7561c915d8c7c9227cf00a0115e5b5c
Tyrone Ting <kfting@nuvoton.com>
2023-07-13 13:55:17 +0800

    drivers: eSPI: add eSPI driver support for NPCM4XX
    Support the Intel Enhanced Serial Peripheral Interface (eSPI)
    Revision 1.0.In addition to Host communication via the peripheral
    channel, it provides virtual wires support, out-of-band communication,
    and device mastering option over the Chipset SPI flash.
    
    The commit also includes:
    
    1. Add eSPI device tree declarations.
    2. Add npcm4xx-espi-vws-map.dtsi to present the relationship between
       eSPI Virtual-Wire signals, eSPI registers, and wake-up input sources.
    3. Zephyr eSPI api implementation.
    4, Add OOB (Out of Band tunneled SMBus) support.
    
    Signed-off-by: Tyrone Ting <kfting@nuvoton.com>

---
+++ include/dt-bindings/espi/npcm4xx_espi.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Nuvoton Technology Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ESPI_NPCM4XX_ESPI_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ESPI_NPCM4XX_ESPI_H_
+
+/* eSPI VW Master to Slave Register Index */
+#define NPCM4XX_VWEVMS0 0
+#define NPCM4XX_VWEVMS1 1
+#define NPCM4XX_VWEVMS2 2
+#define NPCM4XX_VWEVMS3 3
+#define NPCM4XX_VWEVMS4 4
+#define NPCM4XX_VWEVMS5 5
+#define NPCM4XX_VWEVMS6 6
+#define NPCM4XX_VWEVMS7 7
+#define NPCM4XX_VWEVMS8 8
+#define NPCM4XX_VWEVMS9 9
+
+/* eSPI VW Slave to Master Register Index */
+#define NPCM4XX_VWEVSM0 0
+#define NPCM4XX_VWEVSM1 1
+#define NPCM4XX_VWEVSM2 2
+#define NPCM4XX_VWEVSM3 3
+#define NPCM4XX_VWEVSM4 4
+#define NPCM4XX_VWEVSM5 5
+#define NPCM4XX_VWEVSM6 6
+#define NPCM4XX_VWEVSM7 7
+#define NPCM4XX_VWEVSM8 8
+#define NPCM4XX_VWEVSM9 9
+#define NPCM4XX_VWEVSM10 10
+#define NPCM4XX_VWEVSM11 11
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ESPI_NPCM4XX_ESPI_H_ */
